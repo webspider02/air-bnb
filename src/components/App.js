6c447bf3c334155978974c6760c3cd0f687ba7ea
webspider02 <giorgoszampakikas@gmail.com>
2024-07-17 15:11:05 +0300

    hero component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import Header from './Header';
+import Hero from './Hero';
 import '../styles.css';
 
 export default function App() {
     return (
         <>
          <Header/>
+         <Hero/>
         </>
     );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+export default function Hero() {
+    return (
+        <section id="hero">
+            <div className="sadorini-image">
+                <img src="../images/sadorini.jpg" alt="Sadorini"/>
+            </div>
+            
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    );
+}
